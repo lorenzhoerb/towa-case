8d9bc75c1250e97e0b30ad866f9ade5d0d65aa9c
Lorenz Hörburger <e12024737@student.tuwien.ac.at>
2023-06-15 12:05:32 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container bg-gray-300 rounded mx-auto p-2">
+    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl text-gray-900">
+      Hello World</h1>
+  </div>
+</template>
