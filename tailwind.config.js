e66dd60c89e49efbb0a2b77c42d7a387166b5f3c
Lorenz Hörburger <e12024737@student.tuwien.ac.at>
2023-06-16 14:28:35 +0200

    add theme color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const colors = require('tailwindcss/colors');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +6,15 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.violet[600],
+        'primary-light': colors.violet[200],
+        'on-primary': colors.slate[100],
+        'on-primary-light': colors.violet[950],
+        'primary-text': colors.gray[900],
+      }
+    },
   },
   plugins: [],
 }
