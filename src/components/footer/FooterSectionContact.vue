c6270615016a66a5b49216877e9a489a49a27af7
Lorenz Hörburger <e12024737@student.tuwien.ac.at>
2023-06-18 11:20:48 +0200

    add icons in footer contact

--- src/components/footer/FooterSectionContact.vue
+++ src/components/footer/FooterSectionContact.vue
@@ -10,9 +10,24 @@ export default {
 <template>
     <FooterSection title="Contact">
         <ul>
-            <li class="hover:underline mb-1"><a href="tel:+43111111111">+43 111 111 111</a></li>
-            <li class="hover:underline mb-1"><a href="mailto:office@vecotry.com">office@vecotry.com</a></li>
-            <li class="mb-1">Währinger Straße 10<br /> 1180 Wien</li>
+            <li class="hover:underline mb-1">
+                <font-awesome-icon :icon="['fas', 'phone']" class="contact-icon" />
+                <a href="tel:+43111111111">+43 111 111 111</a>
+            </li>
+            <li class="hover:underline mb-1">
+                <font-awesome-icon :icon="['fas', 'envelope']" class="contact-icon" />
+                <a href="mailto:office@vecotry.com">office@vecotry.com</a>
+            </li>
+            <li class="mb-1">
+                <font-awesome-icon :icon="['fas', 'location-dot']" class="contact-icon" />
+                Währinger Straße 10, 1180 Wien
+            </li>
         </ul>
     </FooterSection>
 </template>
+
+<style>
+.contact-icon {
+    @apply mr-2 inline-block;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -11,10 +11,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faPhone, faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faUserSecret)
+library.add(faPhone)
+library.add(faEnvelope)
+library.add(faLocationDot)
 
 const app = createApp(App)
 
